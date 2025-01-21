714deee17f7010a0d0369f35d9d2e96a5823e350
Md. Sowad Karim <43478252+Md-SowadKarim@users.noreply.github.com>
2025-01-21 15:13:42 +0600

    update
    update

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../components/shared/Navbar/Navbar'
-import Footer from '../components/shared/footer/Footer'
+
+
+import Navbar from '../../src/components/shared/navbar/Navbar'
+import Footer from '../../src/components/shared/footer/Footer'
+
 
 const Main = () => {
   return (
