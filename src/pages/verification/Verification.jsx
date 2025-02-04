025187d86396003f6908e732e087b6b50422f0a6
Md. Sowad Karim <43478252+Md-SowadKarim@users.noreply.github.com>
2025-02-04 20:56:34 +0600

    Update Verification.jsx
    comment console problem

--- src/pages/verification/Verification.jsx
+++ src/pages/verification/Verification.jsx
@@ -105,7 +105,7 @@ const data= useLoaderData()
        selector: row => (
            <div >
              <Link to={`/orderDetails/${row.oid}`}>
-              <button className='bg-blue-600 px-4 py-3 rounded-lg text-white' onClick={e=> //console.log("its clicked the edit button")}>Details</button>
+              <button className='bg-blue-600 px-4 py-3 rounded-lg text-white' >Details</button>
               </Link>
            </div>
            )
